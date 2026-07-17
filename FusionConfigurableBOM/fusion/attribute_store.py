ca63c34b4a12dd1094b5a68519542e90e2e4e08e
Garth <12802614+Garth-42@users.noreply.github.com>
2026-07-17 00:27:50 -0500

    Update existing Fusion attributes in place

--- FusionConfigurableBOM/fusion/attribute_store.py
+++ FusionConfigurableBOM/fusion/attribute_store.py
@@ -4,11 +4,21 @@ def read_values(component, field_ids):
     return {field_id: (attribute.value if (attribute := component.attributes.itemByName(FIELD_ATTRIBUTE_GROUP, field_id)) else '') for field_id in field_ids}
 
 def write_value(component, field_id, value):
-    component.attributes.add(FIELD_ATTRIBUTE_GROUP, field_id, str(value))
+    attribute = component.attributes.itemByName(FIELD_ATTRIBUTE_GROUP, field_id)
+    if attribute:
+        # Updating the existing Attribute marks the owning design as modified.
+        # Re-adding an attribute only creates a value reliably when it is absent.
+        attribute.value = str(value)
+    else:
+        component.attributes.add(FIELD_ATTRIBUTE_GROUP, field_id, str(value))
 
 def rename_value(component, old_field_id, new_field_id):
     attribute = component.attributes.itemByName(FIELD_ATTRIBUTE_GROUP, old_field_id)
     if not attribute:
         return
-    component.attributes.add(FIELD_ATTRIBUTE_GROUP, new_field_id, attribute.value)
+    replacement = component.attributes.itemByName(FIELD_ATTRIBUTE_GROUP, new_field_id)
+    if replacement:
+        replacement.value = attribute.value
+    else:
+        component.attributes.add(FIELD_ATTRIBUTE_GROUP, new_field_id, attribute.value)
     attribute.deleteMe()
