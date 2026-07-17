647167924904f12458bf8624e41220051662cff4
Garth <12802614+Garth-42@users.noreply.github.com>
2026-07-16 23:54:13 -0500

    Float BOM palette above Fusion browser

--- FusionConfigurableBOM/fusion/attribute_store.py
+++ FusionConfigurableBOM/fusion/attribute_store.py
@@ -5,3 +5,10 @@ def read_values(component, field_ids):
 
 def write_value(component, field_id, value):
     component.attributes.add(FIELD_ATTRIBUTE_GROUP, field_id, str(value))
+
+def rename_value(component, old_field_id, new_field_id):
+    attribute = component.attributes.itemByName(FIELD_ATTRIBUTE_GROUP, old_field_id)
+    if not attribute:
+        return
+    component.attributes.add(FIELD_ATTRIBUTE_GROUP, new_field_id, attribute.value)
+    attribute.deleteMe()
